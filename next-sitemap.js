51c6386b9b52cdbbafc538c6b8737836530ccde7
parkoon <devparkoon@gmail.com>
2022-04-13 22:11:56 +0900

    next sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+const siteUrl = 'https://parkoon.co.kr' 
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
